5d7427cef12efe8fb67050e8525983a715aded37
Nott-Santiago <18030081@itcelaya.edu.mx>
2023-05-23 23:58:18 -0600

    Desplazamiento hacia la seccion correspondiente

--- app.js
+++ app.js
@@ -38,7 +38,10 @@ addEventListener('DOMContentLoaded', () =>{
         anchor.addEventListener('click', function (e) {
             e.preventDefault();
             const target = document.querySelector(this.getAttribute('href'));
-            
+            target.scrollIntoView({
+                behavior: 'smooth'
+                
+          });
         });
       });
   
