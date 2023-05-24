54648c6eb8141de6407acb40aee0e35a32cbda98
Nott-Santiago <18030081@itcelaya.edu.mx>
2023-05-23 23:57:29 -0600

    Const para obtener el destino despues del clic

--- app.js
+++ app.js
@@ -37,6 +37,7 @@ addEventListener('DOMContentLoaded', () =>{
     document.querySelectorAll('nav a').forEach(anchor => {
         anchor.addEventListener('click', function (e) {
             e.preventDefault();
+            const target = document.querySelector(this.getAttribute('href'));
             
         });
       });
